5bbd7d111e2b0026172eef521c1865a3c4ba2218
moelsaeed05-ctrl <moelsaeed05@gmail.com>
2026-02-23 23:59:08 +0200

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
- apiKey: "ضع المفتاح هنا",
- authDomain: "mit-store-bd580.firebaseapp.com",
- projectId: "mit-store-bd580",
- storageBucket: "mit-store-bd580.firebasestorage.app",
- messagingSenderId: "382577223227",
- appId: "ضع appId هنا"
+apiKey:"",
+authDomain:"mit-store-bd580.firebaseapp.com",
+projectId:"mit-store-bd580",
+storageBucket:"mit-store-bd580.firebasestorage.app",
+appId:""
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
